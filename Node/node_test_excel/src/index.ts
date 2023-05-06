0549eb8ea0d1e8f498dffcd4fb924ac8fe17ec60
Lucas C Noman <lucas.noman@br.ey.com>
2023-05-05 21:06:29 -0300

    build: test new package

--- Node/node_test_excel/src/index.ts
+++ Node/node_test_excel/src/index.ts
@@ -1,11 +1,38 @@
-import { CheckForCorruptSheet } from './RoutineCheckCorruptFiles';
+import path from 'path';
+import { getMapOfExcelPath } from './services/SheetManagement';
+import { pathToBckOutboundMX } from './utils/defaultPaths';
+import xlsx from 'node-xlsx';
+import ExcelJS from 'exceljs';
 
-const basePathFromCE = String.raw`C:\Users\GB675AG\EY\Projeto Samsung Order Mgmt - General\03. Gestão da Rotina\Automações\Ferramentas\BCK\Teste\CE`;
-const basePathToCE = String.raw`C:\Users\GB675AG\EY\Projeto Samsung Order Mgmt - General\03. Gestão da Rotina\Automações\Ferramentas E-NERP\BASE SOLIST\CE`;
-const basePathToOutCE = String.raw`C:\Users\GB675AG\EY\Projeto Samsung Order Mgmt - General\03. Gestão da Rotina\Automações\Ferramentas\BCK\OUTBOUND\CE`;
+const workbook = new ExcelJS.Workbook();
 
-const basePathFromMX = String.raw`C:\Users\GB675AG\EY\Projeto Samsung Order Mgmt - General\03. Gestão da Rotina\Automações\Ferramentas\BCK\Teste\MX`;
-const basePathToMX = String.raw`C:\Users\GB675AG\EY\Projeto Samsung Order Mgmt - General\03. Gestão da Rotina\Automações\Ferramentas E-NERP\BASE SOLIST\MX`;
-const basePathToOutMX = String.raw`C:\Users\GB675AG\EY\Projeto Samsung Order Mgmt - General\03. Gestão da Rotina\Automações\Ferramentas\BCK\OUTBOUND\MX`;
+const mapOfSheets = getMapOfExcelPath(pathToBckOutboundMX);
+console.time();
+outerloop: for (const [folderName, listOfFiles] of mapOfSheets) {
+  for (const fileName of listOfFiles) {
+    if (fileName.toLowerCase().includes('out')) {
+      const outboundFile = path.join(pathToBckOutboundMX, folderName, fileName);
 
-CheckForCorruptSheet(basePathFromMX);
+      const [{ name, data }] = xlsx.parse(outboundFile);
+
+      if (name !== 'Sheet1')
+        console.log(`${folderName} - ${fileName} | Nome: ${name}`);
+      else console.log(`OK: ${folderName} - ${fileName} | Nome: ${name}`);
+
+      // -------------------------------------
+      // Esse é o novo código ainda em teste.
+      // Para usá-lo, é só comentar acima das linhas 16 a 20 e descomentar aqui da 26 a 34
+
+      // workbook.xlsx
+      //   .readFile(outboundFile)
+      //   .then(() => {
+      //     const names = workbook.worksheets.map((worksheet) => worksheet.name);
+
+      //     if (!names.includes('Sheet1'))
+      //       console.log(`${folderName} - ${fileName} | Nomes: ${names}`);
+      //   })
+      //   .catch((error) => console.log(error));
+    }
+  }
+}
+console.timeEnd();
